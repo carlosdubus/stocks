794d55528f274dc4c2037c6d961c2b363215169d
Carlos Dubus <carlos.garciadm@gmail.com>
2021-06-24 21:59:12 -0400

    add date filters to api

--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from stocks.models import Stocks
-
+from dateutil.parser import parse
 
 def search(request):
     query = request.GET
     q = Stocks.objects
     q = q.filter(symbol=query.get("symbol"))
+    if query.get("from"):
+        q = q.filter(date__gte=parse(query.get("from")))
+    if query.get("to"):
+        q = q.filter(date__lte=parse(query.get("to")))
     q = q.order_by('date')
     stocks = list(q.values())
     return JsonResponse(stocks, safe=False)
